68a259c9e2753d59aadce80145c08d2d663c3f74
Leon Wang <leonwang@bu.edu>
2024-03-06 21:01:08 -0500

    debuging stock density

--- code/main.py
+++ code/main.py
@@ -47,21 +47,37 @@ def Callback_RelaxJ(Xi):
     Nfeval += 1
 
 if __name__ == '__main__':
-    return_data = pd.read_excel('../data/Credit_Suisse_Data_17-23.xlsx', sheet_name=0)
-    cet_data = pd.read_excel('../data/Credit_Suisse_Data_17-23.xlsx', sheet_name = 1)
-    B = cet_data['CET-1 ratio'].values
-    J = np.tanh(np.pi * 0.5 - np.pi  * B ) + 1 / np.tan(np.pi * (1-B[0]))
+    #return_data = pd.read_excel('../data/Credit_Suisse_Data_17-23.xlsx', sheet_name=0)
+    #cet_data = pd.read_excel('../data/Credit_Suisse_Data_17-23.xlsx', sheet_name = 1)
+    #B = cet_data['CET-1 ratio'].values
+
+    return_data = pd.read_excel('../data/Credit_Suisse_Data_13-23.xlsx', sheet_name=0).iloc[16:]
+    cet_data = pd.read_excel('../data/Credit_Suisse_Data_13-23.xlsx', sheet_name = 1).iloc[16:]
+
+    B = cet_data['CET-1 ratio (phase-in)'].values
+    J = np.tan(np.pi * 0.5 - np.pi  * B ) + 1 / np.tan(np.pi * (1-B[0]))
     Diff_J = np.diff(J)
 
     KDE_J = KernelDensity(bandwidth= 'silverman').fit(Diff_J.reshape(-1,1))
     J_grids = np.linspace(-3, 3, 200).reshape(-1,1)
     PJ_grids = np.exp(KDE_J.score_samples(J_grids))
-    H = Diff_J + 1.2
+    H = Diff_J - min(Diff_J) + 0.01
 
     KDE_H = KernelDensity(bandwidth= 'silverman').fit(H.reshape(-1,1))
     H_grids = np.linspace(1.1, 1.25, 200).reshape(-1, 1)
     PH_grids = np.exp(KDE_H.score_samples(H_grids))
 
+    # [debug density 2.1.3]
+    # H_grids = np.linspace(0.001, 1, 100)
+    # # Density_J(l, a, b ,t ,x):
+    # #Test_DensityJ_Mine = Density_J(12.020007680203374, 1, 100, T, H_grids)
+    # Test_DensityJ_Weixuan = np.exp(Density_J(27.74, 2, 46.3, 1/4, H))
+    # # Test_DensityJ_Mine = np.exp(Density_RelaxJ(12.020007680203374, 100, 1/4, H_grids))
+    # #Test_DensityJ_Weixuan2 = np.exp(Density_RelaxJ(32.28, 26.95, 1/4, H_grids))
+    # # plt.plot(H_grids, Test_DensityJ_Weixuan1, label='liang')
+    # plt.plot(H_grids, Test_DensityJ_Weixuan, label='weixuan')
+    # plt.legend()
+    # plt.show()
 
     # [plotting]
     fig = plt.figure(figsize=(6, 12))
@@ -70,7 +86,7 @@ if __name__ == '__main__':
     plt.legend()
 
     ax2 = fig.add_subplot(312)
-    ax2.plot(1-B, label = 'B', color = 'red')
+    ax2.plot(B, label = 'B', color = 'red')
     plt.legend()
 
     ax3 = fig.add_subplot(313)
@@ -85,7 +101,9 @@ if __name__ == '__main__':
     Test_DensityJ = Density_J(l, 1, b, T, H)
 
 
-    bounds = [(0, None), (0, None)]
+
+
+    bounds = [(0, 100), (0, 100)]
     init = [0.1, 0.1]
     Nfeval = 1
     res = minimize(optimize_RelaxJ, init, args=(T, H), method='Nelder-Mead', options={'maxiter': 100}, bounds=bounds, callback=Callback_RelaxJ, tol = 0.001)
@@ -93,14 +111,16 @@ if __name__ == '__main__':
     print('lambda: %.4f, beta %.4f' % (l1, b))
 
     #[Re-estimation for alpha = 1...6]
+    sorted_list = []
     for a in range(1, 6):
         Nfeval = 1
         print('==========optimization for a = %d===========' % a)
         res = minimize(optimize_DensityJ, init, args=(a, T, H), method='Nelder-Mead', options={'maxiter': 100}, bounds=bounds,
-                       callback=Callback_RelaxJ, tol=0.001)
+                       callback=Callback_DensityJ, tol=0.001)
         print('lambda: %.4f, beta %.4f' % (res.x[0], res.x[1]))
-
-
+        sorted_list.append([res.fun, *res.x, a])
+    sorted_list = sorted(sorted_list)
+    loss, l1, b, a = sorted_list[0]
     # [For stock estimation]
     RET = return_data['Returns'].values
     plt.plot(RET)
@@ -123,10 +143,7 @@ if __name__ == '__main__':
         final_add = [float(log_m1 + log_m2 + log1[i] + log2[i] + mp.log(pcfd(-a, d_input[i]))) for i in range(len(d_input))]
         return np.array(final_add)
 
-
-
-
-
+    #TODO: check evaluation is correct
     def Density_stock(l1, a, b, mu, sigma, l2, muV, sigmaV, e, x, d= 1/252):
         v = mu + muV/d
         up = np.sqrt(sigma**2 + sigmaV**2/d)
@@ -160,7 +177,7 @@ if __name__ == '__main__':
 
     def callback_stock(Xi):
         global Nfeval, d, RET #TODO: change a_temp
-        print('{0: 4d}     {1:.4f}     {2: .4f}    {3:.4f}    {4:.4f}    {5:.4f}    {6:.4f}    {7:.4f}'.format(Nfeval,
+        print('{0: 4d}     mu:{1:.4f}     sigma:{2: .4f}    l2:{3:.4f}    muV:{4:.4f}    sigmaV:{5:.4f}    e:{6:.4f}    loss:{7:.4f}'.format(Nfeval,
             Xi[0], Xi[1], Xi[2], Xi[3], Xi[4], Xi[5],
             optimize_stock(Xi, x= RET,  l1= 98.1804, a = 5, b = 126.174, d= d)))
         Nfeval += 1
@@ -168,13 +185,16 @@ if __name__ == '__main__':
 
     #l1_temp, a_temp, b_temp, mu, sigma, l2, muV, sigmaV, e = [1] * 9
     #Test_DensityStock = Density_stock(l1_temp, a_temp, b_temp, mu, sigma, l2, muV, sigmaV, e, RET)
-    mu_hat, sigma_hat, l2, muV, sigmaV, e = [0.1, 0.3, 2, -0.1100612, 0.2, 0.18795189]
-    RET_point = np.array([RET[0]])
-    Point_DensityStock = Density_stock(98.1804, 5, 126.174, mu_hat, sigma_hat, l2, muV, sigmaV, e, RET_point, d = 1/252)
+    param = [0.186287, 0.225406, 41.0965, 0.0113117, 0.0343969, 0.822461]
+    mu_hat, sigma_hat, l2, muV, sigmaV, e = param
+
+    RET_point = RET
+    Point_DensityStock = Density_stock(l1, a, b, mu_hat, sigma_hat, l2, muV, sigmaV, e, RET_point, d = 1/252)
+
+    print( np.sum(np.log(Point_DensityStock)), optimize_stock(param, l1=l1, a=a, b=b, d=1 / 252, x=RET))
 
     # mu, sigma, l2, muV, SigmaV, e
     bounds = [(-1, 1), (0.01, 1), (0, None), (-1,1), (0.01, 1), (0, None)]
-    #init = [0, 0.5, l1, 0.01, 0.5, 0.5]
     init = [-0.123329, 0.202993, 4.8, 0.067659, 0.0179964, 1.79029]
     Nfeval = 1
     d = 1/252
